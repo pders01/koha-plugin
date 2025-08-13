485ffca610be2bf2f473d201932b4f958363ef88
Paul Derscheid <paul.derscheid@lmscloud.de>
2025-08-13 21:24:55 +0200

    feat: add support for interactive init

--- scripts/init.pl
+++ scripts/init.pl
@@ -12,6 +12,7 @@ use Readonly        qw( Readonly );
 use Template        ();
 use Term::Choose    qw( choose );
 use Term::UI        ();
+use Term::ReadLine  ();
 use Types::Standard qw();
 use YAML::Tiny      ();
 
@@ -84,9 +85,7 @@ Readonly my $HOOKS => [
 
 sub main() {
     my $metadata = Local::Metadata->new;
-    if ( !$metadata->validate ) {
-        l( 'info', 'aborting init...' ) and return;
-    }
+    _prompt_for_metadata($metadata) or do { l( 'info', 'aborting init...' ) and return };
 
     my $cwd        = cwd;
     my $components = [ split /::/smx, $metadata->name ];
@@ -100,10 +99,6 @@ sub main() {
         l( 'error', "plugin path is not a directory: $path" ) and return;
     }
 
-    if ( $components->@* ) {
-        $metadata->name( join q{ }, $components->@[ 0 .. 1, $CONST->{'INDEX_PROJECT'} ] );
-    }
-
     my $tt = Template->new( { INCLUDE_PATH => 'templates', } );
     if ($Template::ERROR) {
         l( 'error', $Template::ERROR ) and return;
@@ -113,7 +108,8 @@ sub main() {
     my $hooks = [
         choose(
             $HOOKS,
-            {   color => 2,
+            {
+                color => 2,
                 info  =>
                     q{Please choose the hooks you'd like to use in your plugin. Some are grouped: api, opac_online_payment.},
                 prompt => q{Select as many as you like with SPACE, then hit ENTER. :)}
@@ -123,7 +119,8 @@ sub main() {
 
     $tt->process(
         '[a].pm.tt',
-        {   c        => $components->@[ $CONST->{'INDEX_TLD'} ],
+        {
+            c        => $components->@[ $CONST->{'INDEX_TLD'} ],
             b        => $components->@[ $CONST->{'INDEX_ORG'} ],
             a        => $components->@[ $CONST->{'INDEX_PROJECT'} ],
             metadata => $metadata->stringify,
@@ -154,4 +151,88 @@ sub _base_module_path( $path, $name ) {    ## no critic qw(ValuesAndExpressions:
     return join q{.}, $path->sibling($name), 'pm';
 }
 
+sub _prompt_for_metadata($metadata) {    ## no critic qw(Subroutines::ProhibitExcessComplexity)
+    my $term = Term::ReadLine->new('koha-plugin init');
+
+    my $name_pattern = qr/^Koha::Plugin::[[:alnum:]]+::[[:alnum:]]+::[[:alnum:]]+$/smx;
+
+    while (1) {
+        my $name = $term->get_reply(
+            prompt  => 'Plugin package name (Koha::Plugin::<TLD>::<ORG>::<PROJECT>):',
+            default => $metadata->name // q{},
+        );
+        if ( defined $name && $name =~ $name_pattern ) {
+            $metadata->name($name);
+        } else {
+            l( 'warning', 'Invalid name; expected Koha::Plugin::<TLD>::<ORG>::<PROJECT>' );
+            next;
+        }
+
+        my $author = $term->get_reply( prompt => 'Author:', default => $metadata->author // q{} );
+        $metadata->author( $author // q{} );
+
+        my $description = $term->get_reply( prompt => 'Description:', default => $metadata->description // q{} );
+        $metadata->description( $description // q{} );
+
+        my $min_ver = $term->get_reply(
+            prompt  => 'Minimum Koha version (e.g. 22.11.00.000):',
+            default => $metadata->min_koha_version // q{}
+        );
+        $metadata->min_koha_version( $min_ver // q{} );
+
+        my $max_ver = $term->get_reply(
+            prompt  => 'Maximum Koha version (e.g. 25.05.00.000):',
+            default => $metadata->max_koha_version // q{}
+        );
+        $metadata->max_koha_version( $max_ver // q{} );
+
+        my $version = $term->get_reply(
+            prompt  => 'Plugin version (semver, e.g. 0.1.0):',
+            default => $metadata->version // '0.1.0'
+        );
+        $metadata->version( $version // q{} );
+
+        my $date_authored = $term->get_reply(
+            prompt  => 'Date authored (YYYY-MM-DD or today):',
+            default => $metadata->date_authored // 'today'
+        );
+        $metadata->date_authored( $date_authored // 'today' );
+
+        my $date_updated = $term->get_reply(
+            prompt  => 'Date updated (YYYY-MM-DD or today):',
+            default => $metadata->date_updated // 'today'
+        );
+        $metadata->date_updated( $date_updated // 'today' );
+
+        # Derive sensible defaults for optional fields
+        my $release_default = q{};
+        my $static_default  = $metadata->static_dir_name // 'static';
+        my $parts           = [ split /::/smx, ( $metadata->name // q{} ) ];
+        if ( @{$parts} == $CONST->{'LENGTH_COMPONENTS'} ) {
+            my ( undef, undef, undef, $org, $project ) = @{$parts};
+            $release_default = lc join q{-}, $org, $project;
+        }
+
+        my $release_filename = $term->get_reply(
+            prompt  => 'Release filename (basename for .kpz):',
+            default => $metadata->release_filename // $release_default
+        );
+        $metadata->release_filename( $release_filename // $release_default );
+
+        my $static_dir = $term->get_reply( prompt => 'Static directory name:', default => $static_default );
+        $metadata->static_dir_name( $static_dir // $static_default );
+
+        # Validate and loop if errors
+        return 1 if $metadata->validate;
+
+        my $retry = $term->get_reply( prompt => 'Validation failed. Retry? (y/N):', default => 'N' );
+        return 0 if ( ( $retry // 'N' ) =~ /^[Nn]/smx );
+
+    }
+
+    return 1;
+}
+
 main();
+
+1;
